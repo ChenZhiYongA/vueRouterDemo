61acf3a1384e2c637da6b1faa882d4136b6b5689
ChenZhiYongA <2922592097@qq.com>
2021-01-05 12:19:46 +0800

    first commit

---
+++ src/layout/components/navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        temper
+    </div>
+</template>
+
+<script>
+    const data2FromQuery = {
+        data: ''
+    }
+    export default {
+        name: "",
+        data() {
+            return {
+                data: '',
+                data1: [],
+                data2From: Object.assign({}, data2FromQuery),
+            }
+        },
+        mounted: {},
+        created() {
+        },
+        methods: {},
+    }
+</script>
+
+<style scoped>
+
+</style>
